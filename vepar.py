a3d2f5712437bc75c980c090cb55fd42573e5ada
Baptiste Esteban <baptiste.esteban@epita.fr>
2020-01-25 23:45:31 +0100

    [P1]: color conversion finished

--- vepar.py
+++ vepar.py
@@ -3,55 +3,53 @@ import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage import io, color
+from skimage import io, color, img_as_ubyte, img_as_float
 
-def x4(X):
-    res = np.zeros(2 * np.array(X.shape)).astype(np.uint8)
-    res[::2, ::2] = X
-    res[1::2, ::2] = X
-    res[1::2, 1::2] = X
-    res[::2, 1::2] = X
+def yuv_to_rgb(img):
+    """
+    From Video Processing and Communication (2001)
+    """
+    img = img.astype(np.int)
+    res = np.zeros(img.shape).astype(np.float)
+    res[:, :, 0] = 1.164 * (img[:, :, 0] - 16) + 1.596 * (img[:, :, 2] - 128)
+    res[:, :, 1] = 1.164 * (img[:, :, 0] - 16) - 0.392 * (img[:, :, 1] - 128) - 0.813 * (img[:, :, 2] - 128)
+    res[:, :, 2] = 1.164 * (img[:, :, 0] - 16) + 2.017 * (img[:, :, 1] - 128)
     return res
 
-def yuv2rgb(img):
-    res = np.zeros(img.shape).astype(np.uint8)
-
-    Y = img[:, :, 0]
-    U = img[:, :, 1]
-    V = img[:, :, 2]
-
-    res[:, :, 0] = Y + 1.4075 * (V - 128)
-    res[:, :, 1] = Y - 0.3455 * (U - 128) - (0.7169 * (V - 128))
-    res[:, :, 2] = Y + 1.7790 * (U - 128)
+def convert_pgm(img):
+    res = np.zeros((img.shape[0] * 2 // 3, img.shape[1], 3)).astype(np.uint8)
+    res[:, :, 0] = img[:img.shape[0] * 2 // 3, :]
     
-    return res
-
-def pgm_to_ppm(input_img):
-    if (input_img.ndim != 2):
-        raise Exception("PGM2PPM: invalid shapes")
-    (nrows, ncols) = input_img.shape
-    res_img = np.zeros((nrows * 2 // 3, ncols, 3)).astype(np.uint8)
-    # Get the channels
-    Y = input_img[:nrows * 2 // 3, :]
-    U = input_img[nrows * 2 // 3:, :ncols // 2]
-    V = input_img[nrows * 2 // 3:, ncols // 2:]
-
-    res_img[:, :, 0] = Y
-    res_img[:, :, 1] = x4(U)
-    res_img[:, :, 2] = x4(V)
+    U = img[img.shape[0] * 2 // 3:, :img.shape[1] //2]
+    res[::2, ::2, 1] = U
+    res[1::2, ::2, 1] = U
+    res[::2, 1::2, 1] = U
+    res[1::2, 1::2, 1] = U
     
-    return yuv2rgb(res_img)
+    V = img[img.shape[0] * 2 // 3:, img.shape[1] //2:]
+    res[::2, ::2, 2] = V
+    res[1::2, ::2, 2] = V
+    res[::2, 1::2, 2] = V
+    res[1::2, 1::2, 2] = V
+    
+    return yuv_to_rgb(res)
 
-def main():
-    if (len(sys.argv) < 3):
-        print("Usage:", sys.argv[0], " input.pgm output.ppm")
-        exit(1)
-
-    input_img = io.imread(sys.argv[1]).astype(np.uint8)
-    output_img = pgm_to_ppm(input_img)
-    print(output_img.min(), output_img.max())
-    io.imsave(sys.argv[2], output_img)
-    exit(0)
-        
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], "input.pgm output.ppm", file=sys.stderr)
+        exit(1)
+        
+    image_in = img_as_ubyte(io.imread(sys.argv[1]))
+    image_out = convert_pgm(image_in)
+    image_out = (image_out - image_out.min()) / (image_out.max() - image_out.min())
+    image_out = img_as_ubyte(image_out)
+    io.imsave("save.ppm", image_out)  
+    
+    plt.figure(figsize=(20, 10))
+    plt.subplot(131)
+    plt.imshow(image_out[:, :, 0], vmin=0, vmax=255, cmap="gray")
+    plt.subplot(132)
+    plt.imshow(image_out[:, :, 1], vmin=0, vmax=255, cmap="gray")
+    plt.subplot(133)
+    plt.imshow(image_out[:, :, 2], vmin=0, vmax=255, cmap="gray")
+    plt.show()
