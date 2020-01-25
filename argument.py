fbc8e24615a26e6e729baf31aa9a17b8c58dc4c4
Baptiste Esteban <baptiste.esteban@epita.fr>
2020-01-26 00:50:38 +0100

    [P1]: finish sequence

--- argument.py
+++ argument.py
@@ -3,5 +3,6 @@ import argparse
 def parse_argument():
     parser = argparse.ArgumentParser(prog="vepar", description="Utility for MPEG-2")
     parser.add_argument("input", type=str, help="The input directory")
-    parser.add_argument("--save-ppm", help="Save the images in ppm format. The input value is the save directory", default="")
+    parser.add_argument("--save-ppm", help="Save the images in ppm format. The input value is the save directory", action="store_true")
+    parser.add_argument("--cadence", help="Number of image shown by second", type=int, default=100)
     return parser.parse_args()
--- vepar.py
+++ vepar.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
 import sys
+import os
+from glob import glob
+import re
+import shutil
 
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import io
+from tqdm import tqdm
 
 from argument import parse_argument
 from convert import convert
-from glob import glob
-import re
 
 if __name__ == "__main__":
     args = parse_argument()
         
+    print("[+] Initialization")
     filenames = glob(args.input + "/*.pgm")
     filenames = sorted(filenames, key=lambda f: int(re.sub('\D', '', f)))
-    print(type(filenames))
-    for filename in filenames:
+    
+    if args.save_ppm:
+        if os.path.exists("./resulting_ppm"):
+            shutil.rmtree("./resulting_ppm")            
+        os.mkdir("./resulting_ppm")
+        
+    print("[+] Processing")
+    for i in tqdm(range(len(filenames))):
+        filename = filenames[i]
         image_out = convert(filename)
-        if len(args.save_ppm) > 0:
-            io.imsave(args.save_ppm, image_out)
+        if args.save_ppm:
+            io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + ".ppm", image_out)
         else:
             plt.title(filename)
             plt.imshow(image_out)
-            plt.show()
+            plt.draw()
+            plt.pause(1 / args.cadence)
+            plt.clf()
     
     
