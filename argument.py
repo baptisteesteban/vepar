4be8e556a298ba06cf89e60e8d503b83011aa7ab
Baptiste Esteban <baptiste.esteban@epita.fr>
2020-01-27 23:25:22 +0100

    [P1]: finish basis

--- argument.py
+++ argument.py
@@ -3,7 +3,7 @@ import argparse
 def parse_argument():
     parser = argparse.ArgumentParser(prog="vepar", description="Utility for MPEG-2")
     parser.add_argument("input", type=str, help="The input directory")
-    parser.add_argument("--save-ppm", help="Save the images in ppm format. The input value is the save directory", action="store_true")
+    parser.add_argument("--visualize", help="Visualize the video after treatment", action="store_true")
     parser.add_argument("--cadence", help="Number of image shown by second", type=int, default=100)
     parser.add_argument("--config", help="The vepar file exported from mpeg2dec", type=str, default="")
     return parser.parse_args()
--- vepar.py
+++ vepar.py
@@ -31,25 +31,25 @@ if __name__ == "__main__":
             cadence_ips = 27000000 // vepar_file_info.frame_period
         print(str(vepar_file_info))
     
-    if args.save_ppm:
-        if os.path.exists("./resulting_ppm"):
-            shutil.rmtree("./resulting_ppm")            
-        os.mkdir("./resulting_ppm")
+    print("[+] Configure output ppm directory")
+    if os.path.exists("./resulting_ppm"):
+        shutil.rmtree("./resulting_ppm")            
+    os.mkdir("./resulting_ppm")
         
     print("[+] Processing")
     for i in tqdm(range(len(filenames))):
         filename = filenames[i]
         image_out = convert(filename)
         frame1, frame2 = bob_deinterlace(image_out, None)
-        if args.save_ppm:
-            io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_1.ppm", frame1)
-            io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_2.ppm", frame2)
-        else:
-            plt.imshow(frame1)
-            plt.draw()
-            plt.pause(1 / args.cadence)
-            plt.clf()
-            plt.imshow(frame1)
+        io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_1.ppm", frame1)
+        io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_2.ppm", frame2)
+        
+    if args.visualize:
+        filenames = glob("./resulting_ppm/*.ppm")
+        filenames = sorted(filenames, key=lambda f: int(re.sub('\D', '', f)))   
+        for filename in filenames:
+            frame = io.imread(filename)
+            plt.imshow(frame)
             plt.draw()
             plt.pause(1 / args.cadence)
             plt.clf()
