bbc6f2977a6d8a1eaef978b3f2454502ed24df59
Baptiste Esteban <baptiste.esteban@epita.fr>
2020-02-04 16:24:50 +0100

    Change I dont remember

--- bob.py
+++ bob.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-def bob_deinterlace(img, flags):
+def bob_deinterlace(img, flag):
+    if flag == "TOP_FIELD_FIRST" or not flag:
+        return bob_deinterlace_top_field_first(img)
+    else:
+        return img, img
+
+def bob_deinterlace_top_field_first(img):
     frame1, frame2 = np.zeros(img.shape).astype(img.dtype), np.zeros(img.shape).astype(img.dtype)
     frame1[::2] = img[::2]
     frame1[1::2] = img[::2]
--- vepar.py
+++ vepar.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-import sys
 import os
 from glob import glob
 import re
 import shutil
 
-import numpy as np
-import matplotlib.pyplot as plt
 from skimage import io
 from tqdm import tqdm
 
@@ -18,11 +15,11 @@ from visualize import Visualization
 
 if __name__ == "__main__":
     args = parse_argument()
-        
+
     print("[+] Initialization")
     filenames = glob(args.input + "/*.pgm")
     filenames = sorted(filenames, key=lambda f: int(re.sub('\D', '', f)))
-    
+
     vepar_file_info = None
     cadence_ips = 25
     if len(args.config) > 0:
@@ -30,25 +27,24 @@ if __name__ == "__main__":
         vepar_file_info = VeparFile.parse_file(args.config)
         if vepar_file_info.frame_period:
             cadence_ips = 27000000 // vepar_file_info.frame_period
-    
+
     print("[+] Configure output ppm directory")
     if os.path.exists("./resulting_ppm"):
-        shutil.rmtree("./resulting_ppm")            
+        shutil.rmtree("./resulting_ppm")
     os.mkdir("./resulting_ppm")
- 
+
     print("[+] Processing")
     for i in tqdm(range(len(filenames))):
         filename = filenames[i]
         image_out = convert(filename)
-        frame1, frame2 = bob_deinterlace(image_out, None)
+        flag = vepar_file_info.ls_flags[i] if vepar_file_info else None
+        frame1, frame2 = bob_deinterlace(image_out, flag)
         io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_1.ppm", frame1)
         io.imsave("./resulting_ppm/" + str(re.sub('\D', '', filename)) + "_2.ppm", frame2)
 
-    
+
     if args.visualize:
-        v = Visualization()
+        v = Visualization(cadence_ips)
         v.visualize()
-        
+
     print("[+] End")
-    
-    
